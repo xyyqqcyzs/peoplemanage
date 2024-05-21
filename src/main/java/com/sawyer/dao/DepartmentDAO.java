3fb4a14f3ee2adfc0c7a2b2e6554d4c304b956d7
xyyqqcyzs <3258298771@qq.com>
2024-05-21 11:43:55 +0800

    people manage system

--- src/main/java/com/sawyer/dao/DepartmentDAO.java
+++ src/main/java/com/sawyer/dao/DepartmentDAO.java
@@ -1,13 +1,16 @@
 package com.sawyer.dao;
 
 import com.sawyer.entity.Department;
-import com.sawyer.entity.Emp;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
+@Component
 public interface DepartmentDAO {
-    List<Emp> findAll();
+    List<Department> findAll();
 
-    void save(Department dep);
+    void add(Department dep);
 
     void delete(int id);
 
