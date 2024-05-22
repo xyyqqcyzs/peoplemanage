fe67b9926deed550cdf16a6286274881cc33ea9c
xyyqqcyzs <3258298771@qq.com>
2024-05-22 10:16:45 +0800

    Update UserController.java

--- src/main/java/com/sawyer/swagger/controller/UserController.java
+++ src/main/java/com/sawyer/swagger/controller/UserController.java
@@ -41,6 +41,11 @@ public class UserController {
      * @return
      */
     @PostMapping("/register")
+     @PostMapping("/register")
+    public ResponseEntity<String> register(User user) {
+        userService.register(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("注册成功");
+    }
     /*public String register(User user, String code, HttpSession session) {
         String sessionCode = (String) session.getAttribute("code");
         if (sessionCode.equalsIgnoreCase(code)) {
@@ -50,10 +55,7 @@ public class UserController {
             return "redirect:/toRegister";
         }
     }*/
-    public String register(User user) {
-            userService.register(user);
-            return "注册成功";
-    }
+   
 
 
     /**
