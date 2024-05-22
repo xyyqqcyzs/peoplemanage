4d95a1d371c9df4b284013f99b41c3172d884fcf
yanyu <t1006014148@163.com>
2024-05-22 15:48:47 +0800

    tset

---
+++ src/main/java/com/sawyer/test_tsx/testpush.java
@@ -0,0 +1,17 @@
+package com.sawyer.test_tsx;
+
+public class testpush {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public testpush(String name) {
+        this.name = name;
+    }
+}
