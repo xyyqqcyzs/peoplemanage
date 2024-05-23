097832671143a95cb68c28aa6ec5f27b19caa35f
xyyqqcyzs <3258298771@qq.com>
2024-05-23 14:23:58 +0800

    people manage system

--- src/main/java/com/sawyer/entity/LoginRequest.java
+++ src/main/java/com/sawyer/entity/LoginRequest.java
@@ -1,5 +1,14 @@
 package com.sawyer.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginRequest {
     private String account;
     private String password;
