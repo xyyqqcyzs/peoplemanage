a67ea55be84b462503f4bd19b916db47ad0b6976
Lenovo_ydw <2861449974@qq.com>
2024-05-24 10:59:35 +0800

    pushtest

---
+++ peoplemanage-master/src/main/java/com/sawyer/entity/test_ydw.java
@@ -0,0 +1,17 @@
+package com.sawyer.entity;
+
+public class test_ydw {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public test_ydw(String name) {
+        this.name = name;
+    }
+}
