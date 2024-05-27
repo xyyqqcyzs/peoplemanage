29f133a904d6dce39846e04738ad775c5a887258
xyyqqcyzs <3258298771@qq.com>
2024-05-27 09:10:01 +0800

    people manage system

--- src/main/java/com/sawyer/service/EmpmovServiceImpl.java
+++ src/main/java/com/sawyer/service/EmpmovServiceImpl.java
@@ -51,8 +51,8 @@ public class EmpmovServiceImpl implements EmpmovService {
 
     @Override
     @Transactional(propagation = Propagation.SUPPORTS)
-    public List<Employeemov> findby(Integer mov_ID,Date datea, Date dateb,String movtype) {
-        return movDAO.findby(mov_ID,datea,dateb,movtype);
+    public List<Employeemov> findby(Integer emp_ID,Date datea, Date dateb,String movtype) {
+        return movDAO.findby(emp_ID,datea,dateb,movtype);
     }
 
 
